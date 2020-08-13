6e4f3ccdf840c8a230138a427a78e8b0f66630f7
Oscar Alvarez <gavinalv6@gmail.com>
2020-08-13 13:58:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,10 +5,14 @@ class Dog
   end
   
   def name=(dogs_name, dogs_breed = "Mutt")
-    @name
+    @name = dogs_name
+    @breed = dogs_breed
   end
   
   def name 
     @name
   end
+  def breed 
+    @breed 
+  end 
 end 
